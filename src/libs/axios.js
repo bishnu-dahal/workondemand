24e6b20447afae2519600a8651a265ead2c2db3c
Imran Rasheed <imranrasheed.developer@outlook.com>
2023-01-18 20:46:31 +0500

    Super admin panel finalize

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 const axiosIns = axios.create({
   // baseURL: 'http://192.168.100.6:4000',
-  baseURL: 'http://34.203.72.68:4000',
+  baseURL: 'http://www.rizorsiumani.com.mt:4000',
 })
 
 Vue.prototype.$http = axiosIns
