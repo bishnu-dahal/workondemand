0f0a370f3bbd9c47e41294a2d140942fc45f2212
Imran Rasheed <imranrasheed.developer@outlook.com>
2023-01-18 20:47:46 +0500

    base url change

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const axiosIns = axios.create({
-  // baseURL: 'http://192.168.100.6:4000',
+  //     baseURL: 'http://192.168.100.6:4000',
   baseURL: 'http://www.rizorsiumani.com.mt:4000',
 })
 
