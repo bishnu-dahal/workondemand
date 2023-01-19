e9e63962b8adf05c42c4724dbbd806128a656528
Imran Rasheed <imranrasheed.developer@outlook.com>
2023-01-19 19:00:56 +0500

    header chage

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   //     baseURL: 'http://192.168.100.6:4000',
   baseURL: 'http://www.rizorsiumani.com.mt:4000',
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS' },
 })
 
 Vue.prototype.$http = axiosIns
