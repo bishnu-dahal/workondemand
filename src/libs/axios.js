17983f9bb7a919593aa0e13f7a932f42b671729b
Imran Rasheed <imranrasheed.developer@outlook.com>
2023-03-23 22:25:41 +0500

    server

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -5,14 +5,14 @@ import axios from 'axios'
 
 const axiosIns = axios.create({
   baseURL: 'https://app.rizorsiumani.com.mt',
-  withCredentials: false,
-  headers: {
-    "Access-Control-Allow-Origin":"*",
-    "Access-Control-Allow-Methods": "*",
-    'Access-Control-Allow-Headers': "*",
-      "user-agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
-    }
+  // withCredentials: false,
+  // headers: {
+  //   "Access-Control-Allow-Origin":"*",
+  //   "Access-Control-Allow-Methods": "*",
+  //   'Access-Control-Allow-Headers': "*",
+  //     "user-agent":
+  //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+  //   }
   
 })
 
